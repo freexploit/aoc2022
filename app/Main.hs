a605d1d9d6d508f1421d810a99882524e67cfb2f
Christopher Valerio <christopher@valerio.guru>
2022-12-01 09:47:30 -0600

    first day: first commit

---
+++ app/Main.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module Main where
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as B
+import Data.Text.Read as R
+import Data.Either
+import Control.Monad.IO.Class
+
+
+sumInputList :: T.Text -> [Int]
+sumInputList text = 
+    map sum
+    $ map (\ls -> map sum ls)
+    $ filter (\ri -> length ri > 0 )
+    $ map (\li -> map (fst <$>) li ) 
+    $ map (\c -> filter isRight $ map R.decimal c)
+    $ map (\n -> T.splitOn "\n" n) (T.splitOn "\n\n" text ) 
+
+say :: MonadIO m => T.Text -> m ()
+say = liftIO . B.putStrLn
+
+main :: IO ()
+main =
+    B.readFile "input/day1.txt" >>= \text -> 
+        say  "testing" <> T.toText  maximum $ sumInputList text
+        
+
+
+
+
